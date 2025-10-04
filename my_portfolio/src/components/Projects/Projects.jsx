3b05890a8b4201ee3d4b70cd42a42816d7f85442
GundapuDivyasri <divyasrigundapu@gmail.com>
2025-10-04 13:31:43 +0530

    new-pro

--- my_portfolio/src/components/Projects/Projects.jsx
+++ my_portfolio/src/components/Projects/Projects.jsx
@@ -4,12 +4,19 @@ import img1 from "../../assets/r-p-s.jpg";
 import img2 from "../../assets/musicimage.png";
 import img3 from "../../assets/timer.jpg";
 import img4 from "../../assets/turtlerace.jpg";
+import img5 from "../../assets/expensehome.bg.jpg";
 
 const Projects = () => {
   return (
     <div id="Projects" className="p-10 md:p-24 text-white ">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5">
+         <ProjectCard
+          title="Budget Expense Tracker"
+          main="This is a mini web application developed using stack:HTML,CSS,PHP,MYSQL to store and manage the expenses."
+          img={img5}
+          link="https://github.com/GundapuDivyasri/Budget-Expense-Tracker"
+        />
         <ProjectCard
           title="Mini-MusicPlayList"
           main="This is a mini music playlist designed using tech stack:html,css,js,express,mongodb.In this music playlist we can
